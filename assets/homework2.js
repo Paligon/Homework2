785ab738ddd775cd304f2637d891c41e56052013
Vazgen <Vazgen1993664044@gmail.com>
2017-09-11 23:18:41 +0400

    commit my homework2

---
+++ assets/homework2.js
@@ -0,0 +1,21 @@
+let num1 = 1;
+let num2 = 23;
+
+let result;
+let myName = "Vazgen";
+
+let currYear = 2017, mybd = 1993, myAge;
+
+result = num1 + num2;
+console.log('result: num1 + ', result);
+
+num2 = num1 + num2;
+console.log('New value num2: num2: num1 + num2 = ', num2);
+
+num1 = (num1+ num2)/2;
+console.log('New value num1: (num1+ num2)/2 = ', num1);
+
+console.log('My name is: ', myName);
+
+myAge = currYear - mybd;
+console.log('My age is: currYear - mybd: ', myAge);
